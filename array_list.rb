879880a4b71e2b5bb22eca2f2a615ebe759d6180
Eunice Do <eunicedx@gmail.com>
2016-03-04 21:29:38 -0500

    Implement all array list methods

--- array_list.rb
+++ array_list.rb
@@ -1,3 +1,30 @@
 class ArrayList
+  def new(size)
+    @dynamic_arr = Array.new(size)
+  end
 
+  def add(element)
+    @dynamic_arr.push(element)
+    @dynamic_arr[-1]
+  end
+
+  def get(index)
+    raise IndexError if index < 0 || index > @dynamic_arr.length - 1
+    @dynamic_arr[index]
+  end
+
+  def set(index, element)
+    raise IndexError if index < 0 || index > @dynamic_arr.length - 1
+    @dynamic_arr[index] = element
+    @dynamic_arr[index]
+  end
+
+  def length
+    @dynamic_arr.length
+  end
+
+  def insert(index, element)
+    raise IndexError if index < 0 || index > @dynamic_arr.length - 1
+    @dynamic_arr.insert(index, element)
+  end
 end
